414fee49738501c1f83a29d46fd889deac3018a4
DizdarArmin <armin.dizdar@gmail.com>
2021-09-01 12:31:01 +0200

    align sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,14 @@ export default function About() {
     <section className="about container-fluid" id="about">
       <div className="container">
         <div className="row">
-          <div className="col col-12 col-xs-12 col-sm-3 col-md-6 align-center">
+          <div className="col col-12 col-xs-12 col-sm-6 col-md-6 align-center">
             <img
               className="img img-fluid profile"
               src={Image}
               alt="profile"
             ></img>
           </div>
-          <div className="col col-12 col-xs-12 col-sm-9 col-md-6 about-text py-4">
+          <div className="col col-12 col-xs-12 col-sm-6 col-md-6 about-text py-4">
             <h2>About me</h2>
             {getInfoAboutMe()}
           </div>
